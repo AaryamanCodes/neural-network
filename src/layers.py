7a27a7d50d58b5c230fb43c0193a1a6a93118b69
aaryamancodes <aaryamang03@gmail.com>
2026-09-14 22:59:31 +0530

    Rectified Lin. Unit and softmax cross entropy

--- src/layers.py
+++ src/layers.py
@@ -21,3 +21,42 @@ class Linear:
         self.db = dout.sum(axis=0)
         dx = dout @ self.W.T
         return dx
+
+    def params_and_grads(self):
+            return [(self.W, self.dW), (self.b, self.db)]
+
+    class ReLU:
+
+    def __init__(self):
+        self.mask = None
+    def forward(self, z):
+        self.mask = (z > 0)
+        return z * self.mask
+    def backward(self, dout):
+        return dout * self.mask
+
+
+class SoftmaxCrossEntropy:
+
+    def __init__(self):
+        self.probs = None
+        self.y = None
+        self.n = None
+
+    def forward(self, logits, y):
+        shifted = logits - logits.max(axis=1, keepdims=True)
+        exp_scores = np.exp(shifted)
+        probs = exp_scores / exp_scores.sum(axis=1, keepdims=True)
+
+        n = logits.shape[0]
+        log_likelihood = -np.log(probs[np.arange(n), y] + 1e-12)
+        loss = log_likelihood.mean()
+
+        self.probs, self.y, self.n = probs, y, n
+        return loss, probs
+
+    def backward(self):
+        dlogits = self.probs.copy()
+        dlogits[np.arange(self.n), self.y] -= 1
+        dlogits /= self.n
+        return dlogits
